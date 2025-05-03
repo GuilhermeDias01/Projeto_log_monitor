69362371e5c458f759050037cb9f247cccbc5e04
GuilhermeDias01 <guilhermediasgd659@gmail.com>
2025-05-03 18:02:23 -0300

    Ajustando 3

--- log_scan.sh
+++ log_scan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 source config.env
 
 mkdir -p reports
@@ -7,6 +8,14 @@ LOG_REPORT="reports/log_alerts.txt"
 
 echo "[*] Escanenado $LOG_TO_SCAN por falha de login...." > "$LOG_REPORT"
 
-grep "Failed passowrd" "$LOG_TO_SCAN" >> "$LOG_REPORT"
-grep "Authentication failure" "$LOG_TO_SCAN" >> "$LOG_REPORT"
+if grep "Failed password" "$LOG_TO_SCAN" >> "$LOG_REPORT"; then
+  echo "[+] Encontradas falhas de login."
+else
+  echo "[-] Nenhuma falha de login detectada." >> "$LOG_REPORT"
+fi
 
+if grep "authentication failure" "$LOG_TO_SCAN" >> "$LOG_REPORT"; then
+  echo "[+] Encontradas falhas de autenticação."
+else
+  echo "[-] Nenhuma falha de autenticação detectada." >> "$LOG_REPORT"
+fi
